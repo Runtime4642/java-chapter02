ecbfb28e17a46dbf0baad4f6940ef7576a7a1403
Runtime4642 <46149899+Runtime4642@users.noreply.github.com>
2018-12-28 17:03:39 +0900

    Add files via upload

--- src/chapter02/ArrayUtil.java
+++ src/chapter02/ArrayUtil.java
@@ -1,43 +1,33 @@
 package chapter02;
 
 public class ArrayUtil {
-	// int 배열을 double 배열로 변환
-	public static double[] intToDouble( int[] source ) {
-		double[] result = new double[source.length];
-		
-		for(int i = 0; i < source.length; i++) {
-			result[i] = source[i];
-		}
-		
-		return result;
-	}
-	
-	// double 배열을 int 배열로 변환
-	public static int[] doubleToInt( double[] source ) {
-		int[] result = new int[source.length];
-		
-		for(int i = 0; i < source.length; i++) {
-			result[i] = (int)source[i];
-		}
-		
-		return result;
-	}
-	
-	// int 배열 두 개를 연결한 새로운 배열 리턴
-	public static int[] concat( int[] s1, int[] s2 ) {
-		int[] result = new int[s1.length + s2.length];
-		
-		int index = 0;
-		
-		for(int val : s1) {
-			result[index++] = val;
-		}
 
-		for(int val : s2) {
-			result[index++] = val;
+	// int 배열을 double 배열로 변환
+	public static double [] intToDouble( int[] source )
+	{
+		double [] a = new double[source.length];
+		for(int i=0;i<source.length;i++) {
+			a[i]=(double)source[i];
 		}
-		
-		return result;
+		return a;
+	}
+	// double 배열을 int 배열로 변환
+	public static int [] doubleToInt( double[] source )
+	{
+		int [] a = new int[source.length];
+		for(int i=0;i<source.length;i++) 
+			a[i]=(int)source[i];
+		return a;
+	}
+	// int 배열 두 개를 연결한 새로운 배열 리턴
+	public static int [] concat( int[] s1, int[] s2 )
+	{
+		int [] a = new int[s1.length+s2.length];
+		for(int i=0;i<s1.length;i++)
+			a[i] = s1[i];
+		for(int i=s1.length,j=0;i<a.length;i++,j++)
+			a[i] = s2[j];
+		return a;
 	}
 
 }
--- src/chapter02/TV.java
+++ src/chapter02/TV.java
@@ -1,64 +1,70 @@
 package chapter02;
 
 public class TV {
-	private boolean power;
-	private int channel;  //0~50
-	private int volume;   //0~50
+	private int channel; //0~50
+	private int	volume; //0~50
+	private boolean power; //power가 꺼져있으면 볼륨 및 채널이 안돌아감
 	
-	public TV(int channel, int volume, boolean power) {
+	public TV(int channel,int volume,boolean power)
+	{
 		this.channel = channel;
 		this.volume = volume;
 		this.power = power;
 	}
+	public void channel(int channel)
+	{
+		if(power==false)
+		{
+			System.out.println("error TV전원이 꺼져있습니다");
+		}
+		else
+		{
+			if(channel>=0&&channel<=50)
+			this.channel = channel;
+			else if(channel>50)
+				this.channel=0;
+			else 
+				this.channel=50;
+		}
+		
+	}
+	public void channel(boolean up) {
+		if(up)
+			channel(this.channel+1);
+		else
+			channel(this.channel-1);
+	}
+	public void volume(int volume)
+	{
+		if(power==false)
+		{
+			System.out.println("error TV전원이 꺼져있습니다");
+		}
+		else
+		{
+			if(volume>=0&&volume<=50)
+			this.volume = volume;
+			else if(volume>50)
+				this.volume=50;
+			else 
+				this.volume=0;
+		}
+	}
+	public void volume(boolean up) {
+		if(up)
+			volume(this.volume+1);
+		else
+			volume(this.volume-1);
+	}
 	
 	public void power(boolean on) {
-		power = on;
+		if(on)
+		power = true;
+		else
+			power =false;
 	}
-	
-	public void channel(int channel) {
-		if( !power ) {
-			return;
-		}
-		
-		if( channel > 50 ) {
-			channel = 0;
-		}
-		
-		if( channel < 0 ) {
-			channel = 50;
-		}
-
-		this.channel = channel;
-	}
-	
-	public void channel(boolean up) {
-		channel(channel + (up ? 1 : -1));
-	}
-
-	public void volume(int volume) {
-		if( !power ) {
-			return;
-		}
-		
-		if( volume > 50 ) {
-			volume = 0;
-		}
-		
-		if( volume < 0 ) {
-			volume = 50;
-		}
-
-		this.volume = volume;
-	}
-	
-	public void volume(boolean up) {
-		channel(volume + (up ? 1 : -1));
-	}
-	
 	public void status() {
-		System.out.println(
-			"TV[power="  + power   + 
-			", channel=" + channel + 
-			", volume="  + volume  + "]");
+		System.out.println("TV[power="+power+", channel="+channel+" , volume="+volume+"]");
 	}
+
 }
